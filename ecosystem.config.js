3a3671a2ea24fa6d3c72be3da73a10487a29b4ae
KriawqZero <supergamerbr39@gmail.com>
2025-06-27 12:59:39 -0400

    refactor: update ecosystem config for development environment and streamline local start script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,21 @@
 module.exports = {
   apps: [
     {
-      name: 'frontend-v3',
+      name: 'frontend-v3-dev',
+      exec_mode: 'fork',
+      script: 'node_modules/.bin/next',
+      args: ['dev', '--port', '4500'],
+      env: {
+        NODE_ENV: 'development',
+        PORT: 4500
+      }
+    },
+    {
+      name: 'frontend-v3-prod',
       exec_mode: 'cluster',
       instances: 'max',
       script: 'node_modules/next/dist/bin/next',
       args: ['start'],
-      env_local: {
-        NODE_ENV: 'development',
-        PORT: 4500
-      },
-      env_dev: {
-        NODE_ENV: 'development',
-        PORT: 4500
-      },
       env_prod: {
         NODE_ENV: 'production',
         PORT: 4500
