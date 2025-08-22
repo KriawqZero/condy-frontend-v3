0fe7a6700950bdc3799c8464e00e2d1ed46918a3
KriawqZero <supergamerbr39@gmail.com>
2025-08-22 12:12:21 -0400

    feat: adicionar carregamento assíncrono ao formulário de consulta na página do visitante
    - Envolvido o componente ConsultaForm com Suspense para exibir uma mensagem de carregamento enquanto o conteúdo é carregado.

--- src/app/visitante/page.tsx
+++ src/app/visitante/page.tsx
@@ -1,5 +1,6 @@
 import CondyLayout from '@/components/layout/CondyLayout';
 import ConsultaForm from './ConsultaForm';
+import { Suspense } from 'react';
 
 export default function VisitantePage() {
   return (
@@ -13,7 +14,9 @@ export default function VisitantePage() {
 
         <div className="relative pb-20 mx-auto max-w-screen-2xl px-4 sm:px-6 lg:px-8">
           <div className="px-6 py-8">
-            <ConsultaForm />
+            <Suspense fallback={<div className="text-black font-afacad">Carregando...</div>}>
+              <ConsultaForm />
+            </Suspense>
           </div>
         </div>
     </CondyLayout>
